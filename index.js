1b85207a6b86274f6d22dd0a54ede986f611d40f
jkeslar21 <diamondroad91@gmail.com>
2019-03-27 16:02:08 -0700

    mvp

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lambda.sqlite3',
+    },
+    useNullAsDefault: true, // needed for sqlite
+  };
+  const db = knex(knexConfig);
+  
+  const server = express();
+  
+  server.use(helmet());
+  server.use(express.json());
+
+  server.post('/api/cohorts', (req, res) => {
+      db('cohorts')
+      .insert(req.body)
+      .then(ids => {
+          const id = ids[0];
+          db('cohorts')
+            .where({ id })
+            .first()
+            .then(cohort => {
+                res.status(201).json(cohort);
+            })
+            .catch(err => {
+                res.status(500).json(err)
+            })
+      })
+  })
+
+  server.get('/api/cohorts', (req, res) => {
+      db('cohorts')
+        .then(cohorts => {
+            res.status(200).json(cohorts);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+  })
+
+  server.get('/api/cohorts/:id', (req, res) => {
+    const { id } = req.params;
+    db('cohorts')
+    .where({ id })
+    .first()
+    .then(cohort => {
+        if (cohort) {
+          res.status(200).json(cohort)
+        } else {
+          res.status(404).json({ message: 'Cohort Not Found'})
+        }
+      })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  })
+
+  server.get('/api/cohorts/:id/students', (req, res) => {
+    const { id } = req.params
+    db('students') //name of table/db
+        .join('cohorts', 'cohorts.id', 'students.cohort_id') //(table/db to join, whatTOJoin, whatElseToJoin)
+        .select('students.id', 'students.name', 'cohorts.name')//what do we want displayed in new joined table
+        .where('students.cohort_id', id) //we want these to match/locate
+        .then(cohortStudents => {
+            if (cohortStudents.length === 0) {
+                res.status(404).json({ message: 'That student is not in this cohort'})
+            } else {
+                res.status(200).json(cohortStudents)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Request Failed"})
+        })
+  })
+
+  server.delete('/api/cohorts/:id', (req, res) => {
+      db('cohorts')
+      .where({ id: req.params.id })
+      .del()
+      .then(count => {
+        if (count > 0) {
+          res.status(204).end()
+        } else {
+          res.status(404).json({ message: 'Not Found'})
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'error' })
+      })
+  })
+
+  server.put('/api/cohorts/:id', (req, res) => {
+    db('cohorts')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count)
+      } else {
+        res.status(404).json({ message: 'Cohort Not Found'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error'})
+    })
+  })
+
+  server.listen(5000, () =>
+  console.log(`\n** API running on http://localhost:5000 **\n`)
+);
